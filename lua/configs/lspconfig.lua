fe56727434454e3162caff5399b76cbce2f6438e
Jonny Saykosy <jsaykosy@sofya.fr>
2024-03-14 10:14:14 +0100

    migrate custom config from v2.0 to v2.5
    fix: conform format on save
    
    update lazy-lock.json
    
    update lazy-lock.json
    
    vue lsp config
    
    feat: add vue-goto-definition plugin
    
    udpate lazy-lock.json
    
    update lspconfig with template file type
    
    feat: react lspconfig
    
    feat: gitview and neogit
    
    update lazy-lock.json
    
    feat: minimal statusline
    
    feat: rename tsserver to ts_ls
    
    feat: fix eslint_d

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,20 +1,20 @@
--- load defaults i.e lua_lsp
-require("nvchad.configs.lspconfig").defaults()
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
+local util = require "lspconfig/util"
 
--- EXAMPLE
-local servers = { "html", "cssls" }
-local nvlsp = require "nvchad.configs.lspconfig"
+-- local servers = { "html", "cssls" }
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
+-- -- lsps with default config
+-- for _, lsp in ipairs(servers) do
+--   lspconfig[lsp].setup {
+--     on_attach = on_attach,
+--     on_init = on_init,
+--     capabilities = capabilities,
+--   }
+-- end
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,39 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.opt
+o.cursorlineopt = "both" -- to enable cursorline!
+o.swapfile = false
+o.colorcolumn = "80"
+o.pumblend = 15 -- pseudo-transparency of popup-menu
+o.pumheight = 25
+o.fileformats = "unix"
+o.fillchars = {
+  diff = "╱",
+}
+
+local autocmd = vim.api.nvim_create_autocmd
+
+-- dont list quickfix buffers
+autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = "*.fs,*.fsx,*.fsi",
+  command = [[set filetype=fsharp]],
+})
+
+local group_cdpwd = vim.api.nvim_create_augroup("group_cdpwd", { clear = true })
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = group_cdpwd,
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_current_dir(vim.fn.expand "%:p:h")
+  end,
+})
+
+-- Format on save wioth conform.nvim
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format { bufnr = args.buf }
+  end,
+})
