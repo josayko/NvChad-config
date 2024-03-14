0c3b6e276623e1cb6b084e745cefffa3baeb8c72
Jonny Saykosy <jsaykosy@sofya.fr>
2024-03-14 10:14:14 +0100

    feat: custom config v3

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,36 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "window left" })
+map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "window down" })
+map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "window up" })
+map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "window right" })
+
+map("n", "<leader>gsj", "<cmd> GoTagAdd json <CR>", { desc = "Add json struct tags" })
+map("n", "<leader>gsy", "<cmd> GoTagAdd yaml <CR>", { desc = "Add yaml struct tags" })
+
+-- Gisigns
+local gitsigns = require "gitsigns"
+map("n", "<leader>gs", gitsigns.stage_hunk, { desc = "Stage hunk" })
+map("n", "<leader>gr", gitsigns.reset_hunk, { desc = "Reset hunk" })
+map("v", "<leader>gs", function()
+  gitsigns.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
+end, { desc = "Stage hunk" })
+map("v", "<leader>gr", function()
+  gitsigns.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
+end, { desc = "Reset hunk" })
+map("n", "<leader>gS", gitsigns.stage_buffer, { desc = "Stage buffer" })
+map("n", "<leader>gu", gitsigns.undo_stage_hunk, { desc = "Undo stage hunk" })
+map("n", "<leader>gR", gitsigns.reset_buffer, { desc = "Reset buffer" })
+map("n", "<leader>gp", gitsigns.preview_hunk, { desc = "Preview hunk" })
+map("n", "<leader>gb", function()
+  gitsigns.blame_line { full = true }
+end, { desc = "Blame line" })
+map("n", "<leader>tb", gitsigns.toggle_current_line_blame, { desc = "Toggle current line blame" })
+map("n", "<leader>gd", gitsigns.diffthis, { desc = "Diff this" })
+map("n", "<leader>gD", function()
+  gitsigns.diffthis "~"
+end, { desc = "Diff this ~" })
+map("n", "<leader>td", gitsigns.toggle_deleted, { desc = "Toggle deleted" })
+-- Text object
+map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Select hunk" })
--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,47 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.opt
+o.cursorlineopt = "both" -- to enable cursorline!
+o.swapfile = false
+o.colorcolumn = "80"
+o.pumblend = 15 -- pseudo-transparency of popup-menu
+o.pumheight = 25
+o.fileformats = "unix"
+o.fillchars = {
+  diff = "╱",
+}
+o.tabstop = 2
+o.softtabstop = 2
+o.shiftwidth = 2
+o.expandtab = true
+
+local autocmd = vim.api.nvim_create_autocmd
+
+-- dont list quickfix buffers
+autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = "*.fs,*.fsx,*.fsi",
+  command = [[set filetype=fsharp]],
+})
+
+local group_cdpwd = vim.api.nvim_create_augroup("group_cdpwd", { clear = true })
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = group_cdpwd,
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_current_dir(vim.fn.expand "%:p:h")
+  end,
+})
+
+-- Format on save wioth conform.nvim
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format { bufnr = args.buf }
+  end,
+})
+
+vim.g.markdown_fenced_languages = {
+  "ts=typescript",
+}
