8dc912c1504c6110519e843f27bad8f7e419033f
Jonny Saykosy <jsaykosy@sofya.fr>
2024-04-19 12:11:09 +0200

    lua config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -97,13 +97,35 @@ lspconfig.eslint.setup {
 
 lspconfig.lua_ls.setup {
   on_attach = on_attach,
-  on_init = on_init,
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+      return
+    end
+  end,
   capabilities = capabilities,
   settings = {
     Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = "LuaJIT",
+      },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
         globals = { "vim" },
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          -- Depending on the usage, you might want to add additional paths here.
+          -- "${3rd}/luv/library"
+          -- "${3rd}/busted/library",
+        },
+        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+        -- library = vim.api.nvim_get_runtime_file("", true)
       },
     },
   },
@@ -119,6 +141,7 @@ lspconfig.gleam.setup {
 
 lspconfig.emmet_ls.setup {
   on_attach = on_attach,
+  on_init = on_init,
   capabilities = capabilities,
   filetypes = {
     "html",
