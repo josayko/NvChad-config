b054dbc8f7976341832bfa01cca81ba6b205810f
Jonny Saykosy <josayko@pm.me>
2024-05-22 10:37:55 +0200

    update scroll view with obsidian nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -170,4 +170,22 @@ return {
       "nvim-lua/plenary.nvim",
     },
   },
+  {
+    "oflisback/obsidian-bridge.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("obsidian-bridge").setup {
+        obsidian_server_address = "http://localhost:27123",
+        scroll_sync = true,
+      }
+    end,
+    event = {
+      "BufReadPre *.md",
+      "BufNewFile *.md",
+    },
+    lazy = true,
+  },
 }
