b7ac83c92074eb6bb8ce9c94f586c95a3f00753f
Jonny Saykosy <jsaykosy@sofya.fr>
2024-06-03 16:16:22 +0200

    feat: add nvim mini-surround

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -203,4 +203,12 @@ return {
       -- }
     end,
   },
+  {
+    "echasnovski/mini.surround",
+    version = "*",
+    lazy = false,
+    config = function()
+      require("mini.surround").setup()
+    end,
+  },
 }
