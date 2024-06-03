7987e401bc25e8cc02884ba143cddb555015efc4
Jonny Saykosy <josayko@pm.me>
2024-06-03 22:35:17 +0200

    revert vimtex viewer to skim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -195,7 +195,7 @@ return {
     -- tag = "v2.15", -- uncomment to pin to a specific release
     init = function()
       -- VimTeX configuration goes here, e.g.
-      vim.g.vimtex_view_method = "zathura"
+      vim.g.vimtex_view_method = "skim"
       vim.g.vimtex_syntax_enabled = 0
       vim.g.vimtex_indent_enabled = 1
       -- vim.g.vimtex_compile_latexmk_engines = {
