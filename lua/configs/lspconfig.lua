e48504af041b6397fd6cda087707e5055aee6385
Jonny Saykosy <josayko@pm.me>
2024-09-15 19:43:06 +0200

    feat: config volar lsp for vue filetype

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -5,7 +5,7 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 local util = require "lspconfig/util"
 
-local servers = { "lua_ls", "ts_ls" }
+local servers = { "lua_ls", "ts_ls", "emmet_ls", "volar" }
 
 -- -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -77,12 +77,40 @@ lspconfig.ts_ls.setup {
 -- You must make sure volar is setup
 -- e.g. require'lspconfig'.volar.setup{}
 -- See volar's section for more information
+local function get_typescript_server_path(root_dir)
+  local global_ts = "$HOME/.asdf/installs/nodejs/20.15.0/lib/node_modules/typescript"
+  -- Alternative location if installed as root:
+  -- local global_ts = "/usr/local/lib/node_modules/typescript/lib"
+  local found_ts = ""
+  local function check_dir(path)
+    found_ts = util.path.join(path, "node_modules", "typescript", "lib")
+    if util.path.exists(found_ts) then
+      return path
+    end
+  end
+  if util.search_ancestors(root_dir, check_dir) then
+    return found_ts
+  else
+    return global_ts
+  end
+end
+
 lspconfig.volar.setup {
-  init_options = {
-    typescript = {
-      tsdk = "/path/to/.npm/lib/node_modules/typescript/lib",
-      -- Alternative location if installed as root:
-      -- tsdk = '/usr/local/lib/node_modules/typescript/lib'
-    },
+  on_new_config = function(new_config, new_root_dir)
+    new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+    new_config.filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" }
+  end,
+}
+
+lspconfig.emmet_ls.setup {
+  filetypes = {
+    "css",
+    "html",
+    "javascriptreact",
+    "sass",
+    "scss",
+    "typescriptreact",
+    "vue",
+    "htmlangular",
   },
 }
