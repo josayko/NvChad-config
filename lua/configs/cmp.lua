b9c2a8c8a055e0a5ba7f838613a9cea6c808b705
Jonny Saykosy <josayko@pm.me>
2024-10-17 22:02:22 +0200

    add supermaven plugin

---
+++ lua/configs/cmp.lua
@@ -0,0 +1,62 @@
+dofile(vim.g.base46_cache .. "cmp")
+
+local cmp = require "cmp"
+
+local options = {
+  completion = { completeopt = "menu,menuone" },
+
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+
+  mapping = {
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+
+    ["<CR>"] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    },
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      local luasnip = require "luasnip"
+      local suggestion = require "supermaven-nvim.completion_preview"
+      if cmp.visible() then
+        cmp.confirm { select = true }
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif suggestion.has_suggestion() then
+        suggestion.on_accept_suggestion()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif require("luasnip").jumpable(-1) then
+        require("luasnip").jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  },
+
+  sources = {
+    { name = "supermaven" },
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "nvim_lua" },
+    { name = "path" },
+  },
+}
+
+return vim.tbl_deep_extend("force", options, require "nvchad.cmp")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -127,6 +127,12 @@ return {
     end,
   },
   {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      return require "configs.cmp"
+    end,
+  },
+  {
     "vim-crystal/vim-crystal",
     ft = "crystal",
     config = function()
@@ -353,7 +359,24 @@ return {
     "supermaven-inc/supermaven-nvim",
     lazy = false,
     config = function()
-      require("supermaven-nvim").setup {}
+      require("supermaven-nvim").setup {
+        keymaps = {
+          accept_suggestion = "<Tab>",
+          clear_suggestion = "<C-]>",
+          accept_word = "<C-x>",
+        },
+        -- ignore_filetypes = { cpp = true }, -- or { "cpp", }
+        -- color = {
+        --   suggestion_color = "#ffffff",
+        --   cterm = 244,
+        -- },
+        log_level = "info", -- set to "off" to disable logging completely
+        disable_inline_completion = false, -- disables inline completion for use with cmp
+        disable_keymaps = false, -- disables built in keymaps for more manual control
+        condition = function()
+          return false
+        end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+      }
     end,
   },
 }
