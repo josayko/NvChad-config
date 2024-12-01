38dd1dd76d1c15fa63e85e84087e9b715d293955
Jonny Saykosy <josayko@pm.me>
2024-12-02 00:26:16 +0100

    lsp rename mapping

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -162,3 +162,15 @@ lspconfig.pyright.setup {
     },
   },
 }
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+  end,
+})
